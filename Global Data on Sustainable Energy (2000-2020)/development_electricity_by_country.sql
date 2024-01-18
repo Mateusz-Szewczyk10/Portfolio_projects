6c755a8f592f4420051497fda4a25ed771342562
Mateusz Szewczyk <matix9201@gmail.com>
2024-01-18 23:32:23 +0100

    test

--- Global Data on Sustainable Energy (2000-2020)/development_electricity_by_country.sql
+++
@@ -1,4 +0,0 @@
-SELECT (CASE WHEN `Access to electricity (% of population)` < 100 THEN "Electricity developing"
-ELSE "Electricity well-developed" END) as Electricity_status, Entity as "Country", lower(`Access to electricity (% of population)`) as "Lowest status"
-FROM global_data
-GROUP BY Electricity_status, Entity
