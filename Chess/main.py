9ba3f80c6290f412f5d86e57251946dd7c691ef0
Mateusz Szewczyk <matix9201@gmail.com>
2024-07-20 08:31:18 +0200

    ading chess vizs and initial code

--- Chess/main.py
+++ Chess/main.py
@@ -1,3 +1,54 @@
-import pandas as pd
+import pygame
 
-# Load the dataset
+pygame.init()
+WIDTH = 1000
+HEIGHT = 900
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption("Chess Game")
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
+medium_font = pygame.font.Font('freesansbold.ttf', 40)
+big_font = pygame.font.Font('freesansbold.ttf', 50)
+timer = pygame.time.Clock()
+
+# game variables and images
+
+white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+                   (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
+                   (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+captured_pieces_white = []
+captured_pieces_black = []
+# 0 - whites turn no selection: 1-whites turn piece selected: 2- black turn no selection, 3 - black turn piece selected
+turn_step = 0
+
+# load images
+black_queen = pygame.image.load('compononents/Chess_qdt45.svg.png')
+black_queen = pygame.transform.scale(black_queen, (80, 80))
+
+# draw main board
+def draw_board():
+    for i in range(32):
+        column = i % 4
+        row = i // 4
+        if row % 2 == 0:
+            pygame.draw.rect(screen, 'light gray', [600 - (column * 200), row * 100, 100, 100])
+        else:
+            pygame.draw.rect(screen, 'light gray', [700 - (column * 200), row * 100, 100, 100])
+        pygame.draw.rect(screen, 'gray', [0, 800, WIDTH, 100])
+        pygame.draw.rect(screen, 'gold', [0, 800, WIDTH, 100], 5)
+        pygame.draw.rect(screen, 'gold', [800, 0, 200, HEIGHT], 5)
+        status_text = ['White: Select a Piece to Move!', 'White: Select a Destination!',
+                       'Black: Select a Piece to Move!', 'Black: Select a Destination!']
+        screen.blit(big_font.render(status_text[turn_step], True, 'black'), (20, 820))
+        for i in range(9):
+            pygame.draw.line(screen, 'black', (0, 100 * i), (800, 100 * i), 2)
+            pygame.draw.line(screen, 'black', (100 * i, 0), (100 * i, 800), 2)
+        screen.blit(medium_font.render('FORFEIT', True, 'black'), (810, 830))
+
+
+        draw_board()
